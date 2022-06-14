0357729bbd2f2ef338d9bc22b92730008f384fa8
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-06-14 12:05:36 -0300

    Initial component version in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+// Components
+import { Footer } from './components/Footer/Footer';
+import { Main } from './components/Main/Main';
+
+export const App = () => {
+  
+  const [data, setData] = useState([]);
+  const [higher, setHigher] = useState({});
+
+  const getData = () => {
+    fetch('./data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+        higherAmount(data);
+      })
+  };
+
+  const higherAmount = (data) => {
+    let aux = 0;
+    for (let i = 0; i < data.length; i++) {
+      if (aux < data[i].amount) {
+        setHigher(data[i]);
+        aux = data[i].amount;
+      }
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <>
+      <Main data={data} higher={higher}/>
+      <Footer />
+    </>
+  )
+};
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import './Main.styles.css';
+
+export const Main = ({data, higher}) => {
+
+    const handleMouseOut = e => {
+        e.target.innerHTML = "";
+    }
+
+    const handleMouseOver = e => {
+        let newHTMLCode = `
+                    <div class="graphic__number">
+                        <span>$${data[e.target.id-1].amount}</span>
+                    </div>`;
+        e.target.innerHTML = newHTMLCode;
+    }
+
+  return (
+    <main>
+        <header>
+            <div className="header__balance">
+                <h2>My balance</h2>
+                <p>$921.48</p>
+            </div>
+            <div>
+                <div className="circle"></div>
+                <div className="ring"></div>
+            </div>
+        </header>
+    
+        <section>
+            <div>
+                <h3>Spending - Last 7 days</h3>
+                <div className="container__graphics">
+                    {
+                        data.map(day => {
+                            return (<div className={`graphics ${higher === day ? 'higher' : ""}`} key={day.id} id={day.id} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} style={{height: `${day.amount * 3}px`}}></div>)
+                        })
+                    }
+                    {
+                        data.map(day => {
+                            return (<div className='days' key={day.day}>{day.day}</div>)
+                        })
+                    }
+                </div>
+                <div className="total">
+                    <div>
+                        <span>Total this month</span>
+                        <p className="total__number">$478.33</p>
+                    </div>
+                    <div className="total__last-month">
+                        <p>+2.4%</p>
+                        <span>from last month</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+  )
+}
